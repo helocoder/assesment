78b38a047a02fe4448ee4043817e72d38fbecdf3
helocoder <vaidyaabhi3@gmail.com>
2022-10-06 18:17:47 +0530

    UI change a bit

--- src/components/Weather/Front.js
+++ src/components/Weather/Front.js
@@ -2,18 +2,26 @@ import React from 'react'
 import record from '../../Data/data.json'
 import '../../Style/Front-style.css'
 import Lottie from 'lottie-react'
-import animation from '../../Assests/wea.json'
+import animation from '../../Assests/mor.json'
+import top from '../../Assests/wea.json'
 function Front() {
+    
     const defaultOptions = {
         loop: true,
         autoplay: true,
         animationData:animation,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+          },
      };
   return (
     <div className='container'>
 
    {/* -----------------header----------------------- */}
     <div className='header'>
+        <div className='up-anim'>
+        <Lottie animationData={top} loop={true} height={5} width={5}/>
+        </div>
         <div className='head'>
             <h3>{record.query.results.channel.location.city}
              {" "} City {","}{record.query.results.channel.location.region}{", "}{record.query.results.channel.location.country}</h3>
@@ -26,38 +34,64 @@ function Front() {
          <p>Clear</p>
          <p>Day 30℃.Night 20℃</p>
         </div>
-        <div className='anim'>
-         <Lottie options={defaultOptions} height={300} width={300} />
-      </div>
+
 
     </div>
 {/* -----------------Footer--------------------------- */}
 <div className='footer'>
+    <div className='footer-tit'>
     <h4>Today's Forecast for New York City, NY, United States</h4>
+    </div>
     <div className='foot-row-container'>
 
         <div className='morning'>
+        <div className='wea-tit'>
             <h5>Morning</h5>
+            </div>
+            <div className='anim-foot'>
+            <Lottie animationData={animation} loop={true} />
+            </div>
+            <div className='wea-deg'>
             <h4>25℃</h4>
-            <img src=''></img>
+            </div>
         </div>
 
+
         <div className='aftrenoon'>
+            <div className='wea-tit'>
             <h5>Afternoon</h5>
+            </div>
+            <div className='anim-foot'>
+            <Lottie animationData={animation} loop={true} />
+            </div>
+            <div className='wea-deg'>
             <h4>25℃</h4>
-            <img src=''></img>
+            </div>
+           
         </div>
 
         <div className='evening'>
-            <h5>evening</h5>
+        <div className='wea-tit'>
+            <h5>Evening</h5>
+            </div>
+            <div className='anim-foot'>
+            <Lottie animationData={animation} loop={true} />
+            </div>
+            <div className='wea-deg'>
             <h4>25℃</h4>
-            <img src=''></img>
+            </div>
         </div>
 
         <div className='overnight'>
+        <div className='wea-tit'>
             <h5>Overnight</h5>
+            </div>
+            <div className='anim-foot'>
+            <Lottie animationData={animation} loop={true} />
+            </div>
+            <div className='wea-deg'>
             <h4>25℃</h4>
-            <img src=''></img>
+            </div>
         </div>
 
     </div>
